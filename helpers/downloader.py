6b222a69c5a709277f6ebb94393a75250adc8a22
Ajay Thakur <ajaythk.94@gmail.com>
2024-07-23 21:14:43 +0545

    static_file_config_helper_function

---
+++ helpers/downloader.py
@@ -0,0 +1,17 @@
+import requests
+from pathlib import Path
+
+
+def download_to_local(url: str, destination_path: Path, parent_makedir: bool = True):
+    if not isinstance(destination_path, Path):
+        raise ValueError(f'{destination_path} must be a Path object')
+    if parent_makedir:
+        destination_path.parent.mkdir(parents=True, exist_ok=True)
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        destination_path.write_bytes(response.content)
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to download {url}: {e}")
+        return False
