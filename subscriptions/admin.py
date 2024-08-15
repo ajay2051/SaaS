7312361b6c695b0aca9c9a16c6d8f6a0a46de9c6
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-15 21:24:49 +0545

    subscription_column_added

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from subscriptions.models import Subscription
+
 # Register your models here.
+
+admin.site.register(Subscription)
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import Group, Permission
 from django.db import models
 
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
+    group = models.ManyToManyField(Group, null=True, blank=True)
+    permissions = models.ManyToManyField(Permission, null=True, blank=True, limit_choices_to={'content_type__app_label': 'subscriptions'})
 
     class Meta:
         permissions = (
