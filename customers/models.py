559d54494e447cb57c79c41147b42f63565349e9
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-17 15:51:23 +0545

    override_save

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 
+from helpers.billing import create_customer
+
 User = settings.AUTH_USER_MODEL
 
 class Customer(models.Model):
@@ -9,3 +11,11 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.user}"
+
+    def save(self, *args, **kwargs):
+        email = self.user.email
+        if not self.stripe_id:
+            if email != "" or email is not None:
+                stripe_id = create_customer(email=email, raw=True)
+                self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
--- helpers/billing.py
+++ helpers/billing.py
@@ -1,6 +1,13 @@
 import stripe
+
 stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-stripe.Customer.create(
-    name="<NAME>",
-    email="<EMAIL>",
-)
+
+
+def create_customer(name="", email="", raw=False):
+    response = stripe.Customer.create(
+        name=name,
+        email=email,
+    )
+    if raw:
+        return response
+    return response.id  # stripe id
