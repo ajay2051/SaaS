a4d8ab5b41310dbfb91a0fc544a142ef457bfef9
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-17 13:49:07 +0545

    user_subs_sync

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
-
+ALLOW_CUSTOM_GROUPS = True
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
@@ -35,6 +35,22 @@ def user_sub_post_save(sender, instance, created, **kwargs):
     user_sub_instance = instance
     user = user_sub_instance.user
     subscription_obj = user_sub_instance.subscription
-    groups = subscription_obj.groups.all()
-    user.groups.set(groups)
+    groups_id = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list('id', flat=True)
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups_ids)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None:
+            subs_qs = subs_qs.exclude(id=subscription_obj.id)
+        subs_groups = subs_qs.values_list('group__id', flat=True)
+        subs_groups_set = set(subs_groups)
+        # group_ids = groups_ids.values_list('id', flat=True)
+        current_groups = user.groups.all().values_list('id', flat=True)
+        group_ids_set = set(groups_ids)
+        current_groups_set = set(current_groups) - subs_groups_set
+        final_group_ids = list(group_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
 post_save.connect(user_sub_post_save, sender=UserSubscription)
