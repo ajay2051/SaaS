7ef15ec69614c6c618e47143569935dc37360a55
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-17 09:31:18 +0545

    added_usersubs_model_and_signals

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import Group, Permission
 from django.db import models
 
+from django.conf import settings
+from django.db.models.signals import post_save
+
+User = settings.AUTH_USER_MODEL
+
 
 class Subscription(models.Model):
     name = models.CharField(max_length=120, null=True, blank=True)
@@ -14,3 +19,22 @@ class Subscription(models.Model):
             ("pro", "Pro Permissions"),
             ("basic", "Basic Permissions"),
         )
+
+    def __str__(self):
+        return self.name
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.user)
+
+def user_sub_post_save(sender, instance, created, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups = subscription_obj.groups.all()
+    user.groups.set(groups)
+post_save.connect(user_sub_post_save, sender=UserSubscription)
