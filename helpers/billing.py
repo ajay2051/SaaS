3c359ba27482ba1c61c0b0374bf7a96c543039c5
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-18 19:44:20 +0545

    subscription_price_model_added

--- helpers/billing.py
+++ helpers/billing.py
@@ -12,6 +12,7 @@ def create_customer(name="", email="", raw=False):
         return response
     return response.id  # stripe id
 
+
 def create_product(name, metadata={}, raw=False):
     response = stripe.Product.create(name=name, metadata=metadata)
     if raw:
@@ -19,3 +20,12 @@ def create_product(name, metadata={}, raw=False):
     stripe_id = response.id
     return stripe_id
 
+
+def create_price(currency, unit_amount, recurring, product, raw=False):
+    if product is None:
+        return None
+    response = stripe.Price.create(currency=currency, unit_amount=unit_amount, recurring=recurring, product=product, raw=False)
+    if raw:
+        return response
+    stripe_id = response.id
+    return stripe_id
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,9 +1,11 @@
+from decimal import Decimal
+
 from django.conf import settings
 from django.contrib.auth.models import Group, Permission
 from django.db import models
 from django.db.models.signals import post_save
 
-from helpers.billing import create_customer, create_product
+from helpers.billing import create_customer, create_product, create_price
 
 User = settings.AUTH_USER_MODEL
 ALLOW_CUSTOM_GROUPS = True
@@ -11,7 +13,7 @@ ALLOW_CUSTOM_GROUPS = True
 
 class Subscription(models.Model):
     """
-    Subscription = Stripe Model
+    Subscription = Stripe Subscription
     """
     name = models.CharField(max_length=120, null=True, blank=True)
     active = models.BooleanField(default=True)
@@ -33,6 +35,42 @@ class Subscription(models.Model):
         if not self.stripe_id:
                     stripe_id = create_product(name=self.name, raw=False)
                     self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe Price
+    """
+
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = 'MONTHLY', 'Monthly'
+        WEEKLY = 'WEEKLY', 'Weekly'
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, blank=True, null=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    interval = models.CharField(max_length=120, default=IntervalChoices.WEEKLY, choices=IntervalChoices.choices)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+
+    @property
+    def stripe_price(self) -> int:
+        return Decimal(self.price) * 100
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+
+    def save(self, *args, **kwargs):
+        if self.stripe_id is None and self.product_stripe_id is not None:
+            import stripe
+            stripe.api_key = 'sk_test_s15s1cs5c15a4sdxa1xwfw'
+            stripe_id = create_price(currency=self.stripe_currency, unit_amount=self.stripe_price, recurring=self.interval,
+                                 product=self.product_stripe_id)
+            self.stripe_id = stripe_id
         super().save(*args, **kwargs)
 
 
