1529c79e9899a0d1ed52003269dc6341ea254517
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-19 20:03:27 +0545

    model_field_added

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -20,6 +20,8 @@ class Subscription(models.Model):
     group = models.ManyToManyField(Group, null=True, blank=True)
     permissions = models.ManyToManyField(Permission, null=True, blank=True, limit_choices_to={'content_type__app_label': 'subscriptions'})
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
+    featured = models.BooleanField(default=True, help_text="Featured on Django Pricing Page")
+
 
     class Meta:
         permissions = (
@@ -49,6 +51,13 @@ class SubscriptionPrice(models.Model):
     stripe_id = models.CharField(max_length=120, null=True, blank=True)
     interval = models.CharField(max_length=120, default=IntervalChoices.WEEKLY, choices=IntervalChoices.choices)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+    order = models.IntegerField(default=-1, help_text="Ordering on Django pricing Page")
+    featured = models.BooleanField(default=True, help_text="Featured on Django Pricing Page")
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        ordering = ['order', 'timestamp', '-updated']
 
     @property
     def stripe_currency(self):
@@ -72,6 +81,12 @@ class SubscriptionPrice(models.Model):
                                  product=self.product_stripe_id)
             self.stripe_id = stripe_id
         super().save(*args, **kwargs)
+        if self.featured and self.subscription:
+            qs = SubscriptionPrice.objects.filter(
+                subscription=self.subscription,
+                interval=self.interval,
+            ).exclude(id=self.id)
+            qs.update(featured=False)
 
 
 class UserSubscription(models.Model):
